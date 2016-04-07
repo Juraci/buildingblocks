df0be662b2dd13455f6c1e0c4347b6a90c4e4e68
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-06 21:30:41 -0300

    adding constant for port number

--- app.js
+++ app.js
@@ -3,15 +3,19 @@
 (function() {
     'use strict';
 
+    const port = 3000;
+
     let express = require('express');
     let app = express();
 
     app.use(express.static('public'));
 
     app.get('/blocks', function(req, res) {
-        let blocks = ['Fixed', 'Movavle', 'Rotating'];
+        let blocks = ['Fixed', 'Movable', 'Rotating'];
         res.json(blocks);
     });
 
-    app.listen(3000);
+    app.listen(port, function() {
+        console.log(`Listening on port ${port}`)
+    });
 })();
