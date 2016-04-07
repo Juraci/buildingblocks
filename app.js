79f35690d910e7195b86dc679722c8b6e7d45613
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-07 11:57:06 -0300

    keeping the expected limit query behavior behavior

--- app.js
+++ app.js
@@ -9,15 +9,40 @@
     let logger = require('./logger.js');
     let app = express();
 
+    let blocks = {
+        'Fixed': 'Fastened securely in position',
+        'Movable': 'Capable of being moved',
+        'Rotating': 'Moving in a circle around its center'
+    };
+
     app.use(logger);
     app.use(express.static('public'));
 
+    function objectToArray(obj) {
+        let arrayOfProperties = [];
+
+        for(let property in obj) {
+            arrayOfProperties.push(property);
+        }
+
+        return arrayOfProperties;
+    }
+
     app.get('/blocks', function(req, res) {
-        let blocks = ['Fixed', 'Movable', 'Rotating'];
         if (req.query.limit >= 0) {
-            res.json(blocks.slice(0, req.query.limit));
+            res.json(objectToArray(blocks).slice(0, req.query.limit));
         } else {
-            res.json(blocks);
+            res.json(objectToArray(blocks));
+        }
+    });
+
+    app.get('/blocks/:name', function(req, res) {
+        let description = blocks[req.params.name];
+
+        if (!description) {
+            response.status(404).json(`No description found for ${req.params.name}`);
+        } else {
+            res.json(description);
         }
     });
 
